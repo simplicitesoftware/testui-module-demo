3553e068d3e4559dc51ac271ea25ed5ec7ac932e
Simon Campano <scampano@simplicite.fr>
2021-11-30 12:09:48 +0100

    update run script

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# Run tests as in CI pipeline
+# ====================
 sudo docker run -it --rm -v $PWD:/usr/src -w /usr/src markhobson/maven-chrome:jdk-16 mvn test
+
+# Run tests without auto-quit container (debug purposes)
+# ====================
+# sudo docker run -dt --name mvnchr -v $PWD:/usr/src -w /usr/src markhobson/maven-chrome:jdk-16 /bin/bash
+# sudo docker exec -it mvnchr /bin/bash
